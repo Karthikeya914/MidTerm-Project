17d0f96eaf5b90e1bca16d9c3804a9600546fa9a
Karthikeya914 <karthikeya.chakravaram@gmail.com>
2025-01-06 17:13:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const token =
-  "YOUR_SPOTIFY_TOKEN"; // Replace "YOUR_SPOTIFY_TOKEN" with the token you got from the terminal after running the code given in the readme file.
+const token = "YOUR_SPOTIFY_TOKEN"; // Replace with the token from the terminal after running the command which is given in readme file.
 const searchButton = document.getElementById("search-btn");
 const resultContainer = document.getElementById("result-container");
 
@@ -12,26 +11,26 @@ searchButton.addEventListener("click", function () {
   }
 
   fetch(
-    `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-      searchQuery
-    )}&type=track&limit=10`,
+    "https://api.spotify.com/v1/search?q=" + encodeURIComponent(searchQuery) + "&type=track&limit=10",
     {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: "Bearer " + token },
     }
   )
     .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Error in fetching data");
+      }
       return response.json();
     })
     .then(function (data) {
       if (!data.tracks || !data.tracks.items.length) {
-        // Check if no tracks are found
         resultContainer.innerHTML = "<p>No songs found.</p>";
       } else {
         displayResults(data.tracks.items);
       }
     })
     .catch(function () {
-      alert("Please ENTER or UPDATE The Access Token.");
+    alert("Error in fetching data. Please Enter or Update The Access Token");
     });
 });
 
